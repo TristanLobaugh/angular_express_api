cd96123590d9b945b806eabcf4d80e35ff9eec16
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-27 20:54:28 -0400

    updated database to accept votes and update the database

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require("mongodb").MongoClient;
+var mongoUrl = "mongodb://localhost:27017/landmarks";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var db;
+
+MongoClient.connect(mongoUrl, function(error, database){
+	db = database;
 });
+
+router.get('/get_image', function(req, res, next) {
+	db.collection("landmark").find().toArray(function(error, queryResult){
+		res.json(queryResult);
+	});
+});
+
+router.post('/voted', function(req, res, next) {
+  var guess = req.body.votes;
+  db.collection("landmark").find({imgSrc: req.body.image}).toArray(function(error, dbResult){
+  	if(dbResult[0].inusa === guess){
+  		var correct = dbResult[0].voteCorrect + 1;
+  		var incorrect = dbResult[0].voteWrong;
+  		var message = 1;
+  	}else{
+  		var correct = dbResult[0].voteCorrect;
+  		var incorrect = dbResult[0].voteWrong + 1;
+  		var message = 0;
+  	}
+  		db.collection("landmark").updateOne(
+  		{imgSrc: req.body.image},
+  		{
+  			$inc: {"totalVotes": 1},
+  			$set: {"voteCorrect": correct, "voteWrong": incorrect}
+  		});
+  		
+  		res.json(message);
+
+  });
+});
+
+
+//   var dcClass = [
+//   	"Tristan",
+//   	"Josh",
+//   	"Bogdan",
+//   	"Will",
+//   	"Kieth"
+//   ];
+
+//   if(dcClass.indexOf(name) > -1){
+//   	res.json({message: "Hello, " + name + " student in class."});
+//   }else{
+//   res.json({message: "You are not in the class."});
+// 	}
+// });
 
 module.exports = router;
--- front_end/controller.js
+++ front_end/controller.js
@@ -1,9 +1,55 @@
 var searchApp = angular.module("searchApp", []);
 
 searchApp.controller("searchController", function($scope, $http){
+	getImage();
+	var apiUrl = "http://localhost:3000/voted";
 
-	$scope.search = function(){
-		$scope.message = "Hello, World";
+	$scope.vote = function(val){
+		var apiUrl = "http://localhost:3000/voted";
+		$http.post(apiUrl, {"votes": val, image: $scope.image})
+		.then(function successCallback(response){
+		$scope.data = response.data;
+		console.log(response.data);
+
+	  }, function errorCallback(response){
+	  	$scope.result = "ERROR!!! " + response.status;
+	  });
+	  getImage();	
 	}
 
+	function getImage(){
+		var apiUrl = "http://localhost:3000/get_image";
+		$http.get(apiUrl)
+		.then(function successCallback(response){
+			// console.log(response.data);
+		$scope.image = response.data[0].imgSrc;
+	  }, function errorCallback(response){
+	  	$scope.result = "ERROR!!! " + response.status;
+
+	  });	
+	}
+
+
+	// 	$http({
+	// 			method: "GET",
+	// 			url: apiUrl
+	// 		}).then(
+	// 		function successCallback(response){
+	// 		$scope.studentsOnLoad = response.data;
+	// 		console.log($scope.studentsOnLoad)
+	// 	  }, function errorCallback(response){
+	// 	  	$scope.result = "ERROR!!! " + response.status;
+	// 	  });	
+
+	// $scope.search = function(){
+	// 	var apiUrl = "http://localhost:3000/search";
+	// 	$http.post(apiUrl, {"name": $scope.who})
+	// 	.then(function successCallback(response){
+	// 		$scope.data = response.data;
+
+	// 	  }, function errorCallback(response){
+	// 	  	$scope.result = "ERROR!!! " + response.status;
+
+	// 	  });	
+	// }
 });
